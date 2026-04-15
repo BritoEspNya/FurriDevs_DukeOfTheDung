cb045d2f9426e56d6dee3a788a171860ff54584d
Britowo <benjamin.brito.e@uchile.cl>
2026-04-15 12:23:11 -0400

    Switched MultiplayerSyncronizer for InputSyncronizer: send input, not pos

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,18 +9,18 @@ var _data: Statics.PlayerData
 @onready var label: Label = $Label
 @onready var multiplayer_synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
 @onready var camera_2d: Camera2D = $Camera2D
+@onready var input_synchronizer: InputSyncronizer = $InputSynchronizer
 
 func _physics_process(delta: float) -> void:
-	if is_multiplayer_authority():
-		var move_input = Input.get_vector("input_left", "input_right", "input_up", "input_down")
-		velocity.x = move_toward(velocity.x, move_input.x * speed, acceleration * delta)
-		velocity.y = move_toward(velocity.y, move_input.y * speed, acceleration * delta)
-		move_and_slide()
+	var move_input: Vector2 = input_synchronizer.move_input
+	velocity.x = move_toward(velocity.x, move_input.x * speed, acceleration * delta)
+	velocity.y = move_toward(velocity.y, move_input.y * speed, acceleration * delta)
+	move_and_slide()
 
 func setup(data: Statics.PlayerData) -> void:
 	_data = data
 	name = str(data.id)
 	label.text = data.name
 	set_multiplayer_authority(data.id, false)
-	multiplayer_synchronizer.set_multiplayer_authority(data.id, false)
+	input_synchronizer.set_multiplayer_authority(data.id, false)
 	camera_2d.enabled = is_multiplayer_authority()
