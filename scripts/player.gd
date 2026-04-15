aa4d751898a1daabab362f6c4ab1d5d29c6564c9
Britowo <benjamin.brito.e@uchile.cl>
2026-04-15 12:35:41 -0400

    Implemented sync timer for player movement input

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,6 +10,10 @@ var _data: Statics.PlayerData
 @onready var multiplayer_synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
 @onready var camera_2d: Camera2D = $Camera2D
 @onready var input_synchronizer: InputSyncronizer = $InputSynchronizer
+@onready var sync_timer: Timer = $SyncTimer
+
+func _ready() -> void:
+	sync_timer.timeout.connect(_on_sync_timeout)
 
 func _physics_process(delta: float) -> void:
 	var move_input: Vector2 = input_synchronizer.move_input
@@ -24,3 +28,12 @@ func setup(data: Statics.PlayerData) -> void:
 	set_multiplayer_authority(data.id, false)
 	input_synchronizer.set_multiplayer_authority(data.id, false)
 	camera_2d.enabled = is_multiplayer_authority()
+	if is_multiplayer_authority():
+		sync_timer.start()
+		
+@rpc("authority", "call_remote", "unreliable_ordered")
+func send_position(pos: Vector2) -> void:
+	global_position = lerp(global_position, pos, 0.5)
+
+func _on_sync_timeout() -> void:
+	send_position.rpc(global_position)
