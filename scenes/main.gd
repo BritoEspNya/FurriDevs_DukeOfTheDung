3153bf5d07c2147f52f7d908ec82758e960caa6d
Pablo Osorio <pabloosorionavarro@gmail.com>
2026-04-17 01:25:30 -0400

    feat: Implemented Timers and setup MatchController
    Refactor main.gd to spawn players and balls via a new _spawn_match_entities() helper, storing spawned instances in player_instances and ball_instances arrays. Add a match_controller onready var and a _setup_match_controller() method to initialize it with the spawned entities. Update match_scene.tscn to include Players and Balls container nodes, a ShopScreen UI instance, RoundTimer and ShopTimer nodes, and a MatchController node with its script resource.

--- scenes/main.gd
+++ scenes/main.gd
@@ -6,20 +6,36 @@ extends Node2D
 @onready var balls: Node2D = $Balls
 @onready var spawn_points: Node2D = $PlayerSpawnPoints
 @onready var ball_spawn_points: Node2D = $BallSpawnPoints
+@onready var match_controller: MatchController = $MatchController
+
+var player_instances: Array[Node] = []
+var ball_instances: Array[Node] = []
 
 func _ready() -> void:
+	_spawn_match_entities()
+
+func _spawn_match_entities() -> void:
 	for i in Game.players.size():
 		var player_data = Game.players[i]
 		var player_instance = player_scene.instantiate()
 		players.add_child(player_instance, true)
+		
+		player_instances.append(player_instance) # Agregamos el player
+		
 		var player_spawn_point = spawn_points.get_child(i)
 		player_instance.global_position = player_spawn_point.global_position
 		player_instance.setup(player_data)
 		
 		var ball_instance = ball_scene.instantiate()
 		balls.add_child(ball_instance, true)
+		
+		ball_instances.append(ball_instance) # Agregamos la bola
+		
 		var ball_spawn_point = ball_spawn_points.get_child(i)
 		ball_instance.global_position = ball_spawn_point.global_position
+
+func _setup_match_controller() -> void:
+	match_controller.setup(player_instances, ball_instances)
 
 func _process(delta: float) -> void:
 	pass
