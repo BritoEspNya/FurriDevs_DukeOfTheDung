2f03f6f674418baa4fecded5ef791d31f342849e
Britowo <benjamin.brito.e@uchile.cl>
2026-04-22 20:48:42 -0400

    Player flight mode added, syncronized

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,12 @@
 class_name Player
 extends CharacterBody2D
 
-@export var speed: int = 200
+@export var walk_speed: int = 200
+@export var flight_speed: int = 500
 @export var acceleration: float = 400
 
 var _data: Statics.PlayerData
+var _speed: int = walk_speed
 
 @onready var label: Label = $Label
 @onready var multiplayer_synchronizer: MultiplayerSynchronizer = $MultiplayerSynchronizer
@@ -17,8 +19,12 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var move_input: Vector2 = input_synchronizer.move_input
-	velocity.x = move_toward(velocity.x, move_input.x * speed, acceleration * delta)
-	velocity.y = move_toward(velocity.y, move_input.y * speed, acceleration * delta)
+	velocity.x = move_toward(velocity.x, move_input.x * _speed, acceleration * delta)
+	velocity.y = move_toward(velocity.y, move_input.y * _speed, acceleration * delta)
+	if input_synchronizer.flight_input:
+		_speed = flight_speed
+	else:
+		_speed = walk_speed
 	move_and_slide()
 
 func setup(data: Statics.PlayerData) -> void:
