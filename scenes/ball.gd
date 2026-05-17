a07906d1a0e562736d8cde220d92e737bc06d2f8
Benjamin  Brito Espinoza <benjamin.brito.e@ug.uchile.cl>
2026-05-16 20:48:52 -0400

    fix: attach/detach working on clients

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -10,7 +10,6 @@ var follow_speed: float = 8.0
 @onready var area_2d: Area2D = $Area2D
 
 func _ready() -> void:
-	if multiplayer.is_server():
 		area_2d.body_entered.connect(_on_body_entered)
 		area_2d.body_exited.connect(_on_body_exit)
 		
@@ -24,24 +23,30 @@ func _input(event: InputEvent) -> void:
 	var player: Player = Game.get_current_player().scene
 	if event.is_action_pressed("input_movement_mode"):
 		if _near_players.has(player):
-			if attached_player == player:
-				if !is_attached:
-					attach()
-				else:
-					detach()
-				attached_player.input_synchronizer.mode_input = !attached_player.input_synchronizer.mode_input
+			if !is_attached:
+				attach.rpc(player.get_path())
 			else:
-				attached_player = player
+				if player == attached_player:
+					detach.rpc(player.get_path())
+					
+@rpc("any_peer", "call_local", "reliable")
+func attach(player_path: NodePath) -> void:
+	var player: Player = get_node(player_path)
+	if !attached_player:
+		attached_player = player
+	if attached_player == player:
+		set_collision_mask_value(1, false)
+		set_collision_layer_value(1, false)
+		is_attached = true
+		player.input_synchronizer.mode_input = false
 
-func attach() -> void:
-	set_collision_mask_value(1, false)
-	set_collision_layer_value(1, false)
-	is_attached = true
-	
-func detach() -> void:
+@rpc("any_peer", "call_local", "reliable")
+func detach(player_path: NodePath) -> void:
+	var player: Player = get_node(player_path)
 	set_collision_mask_value(1, true)
 	set_collision_layer_value(1, true)
 	is_attached = false
+	player.input_synchronizer.mode_input = true
 
 func _on_body_entered(body: Node2D) -> void:
 	Debug.log("Player entered ball space")
