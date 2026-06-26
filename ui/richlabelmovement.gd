e80681c429e79bf60045c5d0169409eaa4bd42d1
Benjamin  Brito Espinoza <benjamin.brito.e@ug.uchile.cl>
2026-06-26 12:25:57 -0400

    fix: credit scroll speed

--- ui/richlabelmovement.gd
+++ ui/richlabelmovement.gd
@@ -1,11 +1,14 @@
 extends RichTextLabel
 
 @export var speed := 30.0
+var value = 0
 
 func _ready():
 	var sb = get_v_scroll_bar()
 	sb.modulate.a = 0.0
 	sb.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	sb.step = 0.001
 
 func _process(delta):
-	get_v_scroll_bar().value += speed * delta
+	value += speed * delta
+	get_v_scroll_bar().value = value
