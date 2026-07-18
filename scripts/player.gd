c08fdc374f6f5af529aca630319aa81c136a2d59
Pablo Osorio <pabloosorionavarro@gmail.com>
2026-07-17 20:55:33 -0400

    mish 67 lol linear velocity

--- scripts/player.gd
+++ scripts/player.gd
@@ -374,7 +374,8 @@ func increase_dash(value:int) -> void:
 	Debug.log(flight_speed)
 	
 func increase_armor(value:int) -> void:
-	walk_speed = (walk_speed - (20*value))
+	#walk_speed = (walk_speed - (20*value))
+	flight_speed = (flight_speed - (25*value))
 	health_component.armor = value
 # Modifica el daño de todas las armas
 func level_up() -> void:
